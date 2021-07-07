9f58e1111b6c86201a7e2a01a592b44794644d51
Xuanyao Liu <xuanyao.yao@gmail.com>
2021-07-06 20:38:11 -0500

    TPM calculation

--- scripts/get_rdata.r
+++ scripts/get_rdata.r
@@ -10,10 +10,17 @@ median.normalize.log <- function(matrix) {
 count_read=function(x){
  return(length(which(x>=0.5)))
  }
-file.names <- dir("/project2/xuanyao/data/DGN/count/", pattern ="count.txt$",full.names=T)
-all=matrix(0,26584,913)
-for(i in 1:length(file.names)){
- dat=read.table(file.names[i],header=T,as.is=T,sep="\t")
+
+args=commandArgs(trailingOnly=TRUE)
+output=args[1]
+sample_dir=args[2]
+count_dir=args[3]
+
+samples=read.table(sample_dir,header=T)
+N=nrow(samples)
+all=matrix(0,26584,N)
+for(i in 1:N){
+ dat=read.table(paste(count_dir,samples[i,1],".count.txt",sep=""),header=T,as.is=T,sep="\t")
  all[,i]=dat[,7]
  }
 chrom=sapply(strsplit(dat$Chr,";"),"[",1)
@@ -22,46 +29,40 @@ if(length(rm.flag)>0){
 all=all[-rm.flag,]
 dat=dat[-rm.flag,]
 }
-cpm=matrix(0,26584-length(rm.flag),913)
-total_count=apply(all,2,sum)
+cpm=matrix(0,26584-length(rm.flag),N)
 for(i in 1:ncol(all)){
  cpm[,i]=(all[,i]/(total_count[i]/1000000))
  }
 
 ex_count=apply(cpm,1,count_read)
-keep.flag=which(ex_count>=913/2)
+keep.flag=which(ex_count>=N/2)
 
-all=cpm[keep.flag,]
+all=all[keep.flag,]
 dat=dat[keep.flag,]
 
 gene_length=abs(dat$Length)
+rpk=matrix(0,26584-length(rm.flag),N)
 
 for(i in 1:ncol(all)){
-all[,i]=(all[,i]/gene_length)/(total_count[i]/1000000)
+rpk[,i]=(all[,i]/gene_length)
+}
+total_rpk=apply(rpk,2,sum)
+
+for(i in 1:ncol(all)){
+all[,i]=rpk[,i]/(total_rpk/1000000)
 }
 
 #all=median.normalize.log(all)
 library(preprocessCore)
+library(phenix)
+#all <- normalize.quantiles(as.matrix(all))
 all <- normalize.quantiles(as.matrix(all))
 all.t=t(all)
-all2=normalize.quantiles(all.t)
+all2=quantnorm(all.t)
 all=t(all2)
 
-all.norm=matrix(0,nrow(all),ncol(all))
-ex_mean=apply(all,1,mean,na.rm=T)
-for(i in 1:ncol(all)){
-all.norm[,i]=all[,i]-ex_mean
-}
-
-
-ex_sd=apply(all,1,sd,na.rm=T)
-for(i in 1:ncol(all)){
-all.norm[,i]=all.norm[,i]/ex_sd
-}
 #rm.flag=which(is.na(all.norm[,1]))
-all.t=t(all)
-ex.t=all.norm
-ex=t(ex.t)
+ex=t(all)
 gnames=as.character(dat[,1])
 #gnames=gnames[-rm.flag]
-save(all.t,ex, gnames, file="dgn_2qt.rdata")
+save(ex, gnames, file=output)
